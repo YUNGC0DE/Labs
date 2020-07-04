bf462971e8123fdbf724e584862e7bbba60b50d1
yungc0de <evgeniigutin@gmail.com>
2020-07-04 05:41:53 +0300

    braindead strategies added

--- c++/lab5/main.cpp
+++ c++/lab5/main.cpp
@@ -7,12 +7,16 @@
 
 
 int main() {
-
+    //to play my game >_< use A4, B6  checker input format
+    auto player1 = std::make_shared<computer_strategy_t_2>("Stupid Machine");
+    auto player2 = std::make_shared<human_strategy_t>("Smart Human");
+    game_t game(player1, player2);
+    game.play();
+    /*
+    auto player1 = std::make_shared<computer_strategy_t_2>("First player");
     auto player2 = std::make_shared<computer_strategy_t_1>("Second player");
 
-    auto player1 = std::make_shared<computer_strategy_t_2>("First player");
-//    auto player2 = std::make_shared<human_strategy_t>("Second player");
-//
+
     for(int i = 0; i<1000; i++){
         game_t game(player1, player2);
         game.play();
@@ -20,5 +24,6 @@ int main() {
 
     player1->print_stat();
     player2->print_stat();
+    */
     return 0;
 }
---
+++ c++/lab5/strategies/computer_second_strategy.cpp
@@ -0,0 +1,201 @@
+#include "computer_second_strategy.h"
+
+#include <cassert>
+#include <iostream>
+#include <random>
+#include <vector>
+
+
+computer_strategy_t_2::computer_strategy_t_2(std::string name) : name(std::move(name)) {}
+
+step_t computer_strategy_t_2::make_first_step(const field_t &fld) {
+    char letter, capitalLetter, oppositeLetter, oppositeCapitalLetter;
+    std::set<std::pair<int, int>> kill;
+    std::set<std::pair<int, int>> make_step;
+    std::pair<int, int> selected_checker;
+    std::pair<int, int> selected_target;
+    if (player_num == 0){letter = 'w'; capitalLetter = 'W'; oppositeLetter = 'b'; oppositeCapitalLetter = 'B';}
+    else{letter = 'b'; capitalLetter = 'B'; oppositeLetter = 'w'; oppositeCapitalLetter = 'W';}
+    bool changed;
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            if (fld.fld[row][col] == letter || fld.fld[row][col] == capitalLetter) {
+                changed = attacked_checkers(fld, kill, std::pair<size_t, size_t>(col, row));
+
+                if (!changed) {
+                    if(player_num == 0){
+                        if ((fld.fld[row + 1][col + 1] == '*' || fld.fld[row + 1][col - 1] == '*') &&
+                            fld.fld[row][col] == letter) {
+                            make_step.insert(std::pair<size_t, size_t>(col, row));
+                        }
+                        if ((fld.fld[row + 1][col + 1] == '*' || fld.fld[row + 1][col - 1] == '*' ||
+                             fld.fld[row - 1][col + 1] == '*' || fld.fld[row - 1][col - 1] == '*') &&
+                            fld.fld[row][col] == capitalLetter) {
+                            make_step.insert(std::pair<size_t, size_t>(col, row));
+                        }
+                    } else{
+                        if ((fld.fld[row - 1][col + 1] == '*' || fld.fld[row - 1][col - 1] == '*') &&
+                            fld.fld[row][col] == letter) {
+                            make_step.insert(std::pair<size_t, size_t>(col, row));
+                        }
+                        if ((fld.fld[row + 1][col + 1] == '*' || fld.fld[row + 1][col - 1] == '*' ||
+                             fld.fld[row - 1][col + 1] == '*' || fld.fld[row - 1][col - 1] == '*') &&
+                            fld.fld[row][col] == capitalLetter) {
+                            make_step.insert(std::pair<size_t, size_t>(col, row));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    std::vector<std::pair<int, int>> checkMoves;
+    if (!kill.empty()) {
+        std::vector<std::pair<int, int>> possible_checkers(kill.begin(), kill.end());
+        //да, это гениальная стратегия, я знаю
+        std::shuffle(possible_checkers.begin(), possible_checkers.end(), std::mt19937(std::random_device()()));
+        selected_checker = possible_checkers.front();
+        if (fld.fld[selected_checker.second][selected_checker.first] == capitalLetter ||
+            fld.fld[selected_checker.second][selected_checker.first] == letter) {
+            int length;
+            if (fld.fld[selected_checker.second][selected_checker.first] == capitalLetter) {
+                length = std::max(selected_checker.second, 7 - selected_checker.second);
+            } else {length = 2;}
+            int change_step[2] = {-1, 1};
+            for (auto &i: change_step) {
+                for (auto &j: change_step) {
+                    bool enemy = false;
+                    for (int point = 1; point <= length; ++point) {
+                        auto x = selected_checker.second + i * point;
+                        auto y = selected_checker.first + j * point;
+                        if (enemy && fld.fld[x][y] != '*') {break;}
+                        if (fld.fld[x][y] == oppositeLetter || fld.fld[x][y] == oppositeCapitalLetter) {enemy = true;}
+                        if (enemy && fld.fld[x][y] == '*') {checkMoves.emplace_back(y,x);}
+                    }
+                }
+            }
+        }
+        std::shuffle(checkMoves.begin(), checkMoves.end(), std::mt19937(std::random_device()()));
+        selected_target = checkMoves.front();
+    } else if (!make_step.empty()) {
+        std::vector<std::pair<int, int>> possible_checkers(make_step.begin(), make_step.end());
+        std::shuffle(possible_checkers.begin(), possible_checkers.end(), std::mt19937(std::random_device()()));
+        selected_checker = possible_checkers.front();
+        if(player_num == 0){
+            if (fld.fld[selected_checker.second][selected_checker.first] == letter) {
+                if (fld.fld[selected_checker.second + 1][selected_checker.first + 1] == '*') {
+                    checkMoves.emplace_back(selected_checker.first + 1, selected_checker.second + 1);
+                } else {
+                    checkMoves.emplace_back(selected_checker.first - 1, selected_checker.second + 1);
+                }
+            }
+        } else{
+            if (fld.fld[selected_checker.second][selected_checker.first] == letter) {
+                if (fld.fld[selected_checker.second - 1][selected_checker.first + 1] == '*') {
+                    checkMoves.emplace_back(selected_checker.first + 1, selected_checker.second - 1);
+                } else {
+                    checkMoves.emplace_back(selected_checker.first - 1, selected_checker.second - 1);
+                }
+            }
+        }
+        if (fld.fld[selected_checker.second][selected_checker.first] == capitalLetter) {
+            size_t length = std::max(selected_checker.second - 0, 7 - selected_checker.second);
+            int change_step[2] = {-1, 1};
+            for (auto &i: change_step) {
+                for (auto &j: change_step) {
+                    for (int point = 1; point < length; ++point) {
+                        if (fld.fld[selected_checker.second + i * point][selected_checker.first + j * point] == '*') {
+                            checkMoves.emplace_back(selected_checker.first + j * point,
+                                                    selected_checker.second + i * point);
+                        } else {break;}
+                    }
+                }
+            }
+        }
+        std::shuffle(checkMoves.begin(), checkMoves.end(), std::mt19937(std::random_device()()));
+        selected_target = checkMoves.front();
+    }
+    return {selected_checker.first + 1, selected_checker.second + 1, selected_target.first + 1,
+            selected_target.second + 1};
+}
+
+bool computer_strategy_t_2::check_attack(const field_t &fld, std::set<std::pair<int, int> > &attack_checker) {
+    attack_checker.clear();
+    bool changed = false;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (player_num == 0) {
+                if (fld.fld[i][j] == 'w' || fld.fld[i][j] == 'W') {
+                    if (attacked_checkers(fld, attack_checker, std::pair<size_t, size_t>(j, i))) {
+                        changed = true;
+                    }
+                }
+            } else {
+                if (fld.fld[i][j] == 'b' || fld.fld[i][j] == 'B') {
+                    if (attacked_checkers(fld, attack_checker, std::pair<size_t, size_t>(j, i))) {
+                        changed = true;
+                    }
+                }
+            }
+        }
+    }
+    return changed;
+}
+
+step_t computer_strategy_t_2::make_next_step(const field_t &fld, std::pair<size_t, size_t> current_checker) {
+    char letter, capitalLetter, oppositeLetter, oppositeCapitalLetter;
+    if (player_num == 0){letter = 'w'; capitalLetter = 'W'; oppositeLetter = 'b'; oppositeCapitalLetter = 'B';}
+    else{letter = 'b'; capitalLetter = 'B'; oppositeLetter = 'w'; oppositeCapitalLetter = 'W';}
+    std::pair<int, int> selected_checker;
+    std::pair<int, int> selected_target;
+    std::vector<std::pair<int, int>> possible_moves;
+    selected_checker.first = current_checker.first - 1;
+    selected_checker.second = current_checker.second - 1;
+    if (fld.fld[selected_checker.second][selected_checker.first] == letter) {
+        int change_step[2] = {-1, 1};
+        for (auto &i: change_step) {
+            for (auto &j: change_step) {
+                if (fld.fld[selected_checker.second + i][selected_checker.first + j] == oppositeLetter ||
+                    fld.fld[selected_checker.second + i][selected_checker.first + j] == oppositeCapitalLetter) {
+                    if (fld.fld[selected_checker.second + i * 2][selected_checker.first + j * 2] == '*') {
+                        possible_moves.emplace_back(selected_checker.first + j * 2,
+                                                    selected_checker.second + i * 2);
+
+                    }
+                }
+            }
+        }
+    }
+    if (fld.fld[selected_checker.second][selected_checker.first] == capitalLetter) {
+        size_t length = std::max(selected_checker.second, 7 - selected_checker.second);
+        int change_step[2] = {-1, 1};
+        for (auto &i: change_step) {
+            for (auto &j: change_step) {
+                bool enemy = false;
+                for (int point = 1; point <= length; ++point) {
+                    if (enemy &&
+                        fld.fld[selected_checker.second + i * point][selected_checker.first + j * point] != '*') {
+                        break;
+                    }
+                    if (fld.fld[selected_checker.second + i * point][selected_checker.first + j * point] == oppositeLetter ||
+                        fld.fld[selected_checker.second + i * point][selected_checker.first + j * point] == oppositeCapitalLetter) {
+                        enemy = true;
+                    }
+                    if (enemy &&
+                        fld.fld[selected_checker.second + i * point][selected_checker.first + j * point] == '*') {
+                        possible_moves.emplace_back(selected_checker.first + j * point,
+                                                    selected_checker.second + i * point);
+                    }
+                }
+            }
+        }
+    }
+    std::shuffle(possible_moves.begin(), possible_moves.end(), std::mt19937(std::random_device()()));
+    selected_target = possible_moves.front();
+    return {selected_checker.first + 1, selected_checker.second + 1, selected_target.first + 1,
+            selected_target.second + 1};
+}
+
+void computer_strategy_t_2::print_stat() const {
+    std::cout << "Computer " << player_num + 1 << ": " << std::endl;
+    computer_interface_t::print_stat();
+}
